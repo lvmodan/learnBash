484441f786e1a719381b390957793e695bc16cd8
lvmodan <amygithub@126.com>
2018-12-19 20:15:43 +0800

    add dd command example

---
+++ chapter3files/files.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Author: Lyndon
+# Date: 12/19/2018
+# File name: file.sh
+# Description: command related with files
+
+# Create a file of  specific size
+dd if=/dev/zero of=junk.data bs=1M count=1
+
